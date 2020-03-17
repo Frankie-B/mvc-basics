eb10bc73ea39a3a3fd2eabb48aabdb7e15e02785
Francis Bukenya <frankie.bukenya@gmail.com>
2020-03-17 19:30:04 +0100

    Add director and year

--- routes/movies.js
+++ routes/movies.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const Movie = require('../models/Movie');
 
-// Displaying all the recipes
+// Displaying ALL the movies from the database
 app.get('/movies', (req, res) => {
   Movie.find({}).then(moviesData => {
     res.render('movies', { moviesHbs: moviesData });
+  });
+});
+
+// Displaying the details for a single movie from the database
+app.get('/movie/details/:movieId', (req, res) => {
+  Movie.findById(req.params.movieId).then(movieData => {
+    res.render('/movie', { movieHbs: movieData });
   });
 });
 
